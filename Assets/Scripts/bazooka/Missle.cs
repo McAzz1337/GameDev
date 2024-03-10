2608494d1a1aeb1112e95b5fcea1fc976c0b140d
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-03-10 04:23:04 +0100

    Added explosion effect

--- Assets/Scripts/bazooka/Missle.cs
+++ Assets/Scripts/bazooka/Missle.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Missle : MonoBehaviour
 {
 
 
+    [SerializeField] private GameObject explosionPrefab;
     [SerializeField] private GameObject blastRadiusPrefab;
     [SerializeField] private float force;
     void Start()
@@ -19,10 +22,10 @@ public class Missle : MonoBehaviour
 
     }
 
-    void OnCollisionEnter(Collision collider)
+    void OnCollisionEnter(Collision collision)
     {
 
-        int layer = 1 << collider.gameObject.layer;
+        int layer = 1 << collision.gameObject.layer;
         if (layer == LayerMask.GetMask("Wall") ||
             layer == LayerMask.GetMask("Ground") ||
             layer == LayerMask.GetMask("Player") ||
@@ -30,6 +33,12 @@ public class Missle : MonoBehaviour
         {
 
             explode();
+
+            expodeClientRpc(
+                collision.contacts[0].point,
+                GetComponent<Rigidbody>().velocity.normalized,
+                collision.contacts[0].normal
+            );
         }
     }
 
@@ -39,6 +48,17 @@ public class Missle : MonoBehaviour
         GameObject g = Instantiate(blastRadiusPrefab, transform.position, transform.rotation);
         g.layer = LayerMask.NameToLayer("Damaging");
         Destroy(gameObject);
+
+    }
+
+    [ClientRpc]
+    public void expodeClientRpc(Vector3 position, Vector3 direction, Vector3 normal)
+    {
+
+        Quaternion rot = Quaternion.LookRotation(Vector3.Cross(-direction, normal), normal);
+        float angle = Vector3.Angle(-direction, normal);
+        position -= direction * Mathf.Sin(angle) * 2;
+        Instantiate(explosionPrefab, position, rot);
     }
 
 }
