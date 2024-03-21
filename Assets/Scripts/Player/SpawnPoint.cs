e83a572f34eea5bedf8e236284c6a1aa9dfe5134
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-03-21 22:01:07 +0100

    Added SpawnPoint to assist PlayerSpawner in assigning a spot to the player at round start

---
+++ Assets/Scripts/Player/SpawnPoint.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class SpawnPoint : NetworkBehaviour
+{
+
+    [SerializeField]
+    private NetworkVariable<bool> occupied =
+                            new NetworkVariable<bool>(
+                                false,
+                                NetworkVariableReadPermission.Everyone,
+                                NetworkVariableWritePermission.Server
+                            );
+
+    public bool acceptPlayer(PlayerNetwork player)
+    {
+
+        if (occupied.Value) return false;
+
+        player.transform.position = transform.position;
+
+        occupied.Value = true;
+
+        return true;
+    }
+
+    public bool isOccupied()
+    {
+
+        return occupied.Value;
+    }
+}
