eedf46b2c837625f12a3e7346f666bf5d3c36094
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-03-21 23:12:12 +0100

    Added RoundManager to handle start of a round

---
+++ Assets/Scripts/Core/RoundManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Services.Lobbies;
+using UnityEngine;
+
+public class RoundManager : NetworkBehaviour
+{
+
+
+
+    void Start()
+    {
+        if (!IsHost) return;
+
+        for (int i = 0; i < NetworkManager.Singleton.ConnectedClients.Count; i++)
+        {
+
+            NetworkClient client = NetworkManager.Singleton.ConnectedClients[(ulong)i];
+            PlayerNetwork player = client.PlayerObject.GetComponent<PlayerNetwork>();
+            player.enableBattleControls();
+
+            PlayerSpawner.instance.SpawnPlayer(player);
+        }
+    }
+
+
+}
