44ea0b20fb8bf8c22b8a52a39c0cf5d77c63b804
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-04-01 18:58:20 +0200

    Segragated everything considering weapons to WeaponHolder class

--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -10,31 +10,12 @@ public class PlayerNetwork : NetworkBehaviour
 
     public static PlayerNetwork localPlayer;
 
-    [Header("References")]
-    [SerializeField] private Transform weaponTransform;
 
     [Header("Debug Info")]
     [SerializeField] private bool isHost;
     [SerializeField] private bool isOwner;
     [SerializeField] private bool isClient;
-    [SerializeField] private WeaponNetwork weapon;
-    [SerializeField]
-    private NetworkVariable<EWeapon> weaponType =
-                            new NetworkVariable<EWeapon>(
-                                EWeapon.NONE,
-                                NetworkVariableReadPermission.Everyone,
-                                NetworkVariableWritePermission.Server);
 
-
-    private NetworkVariable<Vector2> moveInput =
-        new NetworkVariable<Vector2>(Vector2.zero,
-        NetworkVariableReadPermission.Everyone,
-        NetworkVariableWritePermission.Owner);
-
-
-    Rigidbody rb;
-
-    PlayerControls controls;
 
     public delegate void UseCallback();
     private UseCallback onUse;
@@ -42,11 +23,29 @@ public class PlayerNetwork : NetworkBehaviour
     void Awake()
     {
 
-        controls = new PlayerControls();
-        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
     }
 
 
+    void Start()
+    {
+
+        if (IsOwner)
+        {
+
+            localPlayer = this;
+            GetComponent<IDHolder>().setClientID(NetworkManager.Singleton.LocalClientId);
+        }
+
+        isHost = IsHost;
+        isOwner = IsOwner;
+        isClient = IsClient;
+    }
 
     public void enableRenderer()
     {
@@ -80,82 +79,6 @@ public class PlayerNetwork : NetworkBehaviour
 
 
 
-    public override void OnNetworkSpawn()
-    {
-        base.OnNetworkSpawn();
 
-    }
-
-    void Start()
-    {
-
-        if (IsOwner)
-        {
-
-            localPlayer = this;
-            GetComponent<IDHolder>().setClientID(NetworkManager.Singleton.LocalClientId);
-        }
-
-        isHost = IsHost;
-        isOwner = IsOwner;
-        isClient = IsClient;
-
-    }
-
-
-
-
-    public void shoot()
-    {
-
-
-        if (weapon == null) return;
-
-        weapon.shoot();
-
-        if (weapon.isEmpty())
-        {
-
-            dropWeapon();
-        }
-        else if (weapon as MolotovCocktailMock != null)
-        {
-
-            weapon = null;
-        }
-    }
-
-
-
-    public void pickupWeapon(WeaponNetwork weapon)
-    {
-
-        this.weapon = weapon;
-        weaponType.Value = weapon.getIdentifier();
-        weapon.transform.position = weaponTransform.position;
-        weapon.transform.rotation = weaponTransform.rotation;
-        weapon.transform.SetParent(transform);
-
-        ulong clientID = GetComponent<IDHolder>().getClientID();
-        Debug.Log("ID ON PICKUP: " + clientID);
-        weapon.GetComponent<IDHolder>().setClientID(clientID);
-    }
-
-
-
-
-    public void dropWeapon()
-    {
-
-        weapon.drop();
-        weapon = null;
-    }
-
-
-    public bool canPickupWeapon()
-    {
-
-        return weapon == null;
-    }
 
 }
